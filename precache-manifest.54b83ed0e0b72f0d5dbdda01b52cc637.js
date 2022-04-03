1bf96ca2da11bee7a88bc2b99342ef378585b507
omg8hk <omg8hkserver@gmail.com>
2022-04-03 20:21:52 +0800

    Revert "Updates"
    This reverts commit 605527ec6a22f947f4a81f52224d072d0ae25723.

---
+++ precache-manifest.54b83ed0e0b72f0d5dbdda01b52cc637.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c9cd5092670889759d2e5977a72ad64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d161000a43014f61776",
+    "url": "/static/css/2.9905d55a.chunk.css"
+  },
+  {
+    "revision": "db2a3c77a8b47e6b3aea",
+    "url": "/static/css/main.ae877ffd.chunk.css"
+  },
+  {
+    "revision": "1d161000a43014f61776",
+    "url": "/static/js/2.f0b8041e.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/static/js/2.f0b8041e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db2a3c77a8b47e6b3aea",
+    "url": "/static/js/main.f47173e8.chunk.js"
+  },
+  {
+    "revision": "7fd8f8b23b79231f3b83",
+    "url": "/static/js/runtime-main.933364b2.js"
+  },
+  {
+    "revision": "9855b418ee3f44f42d308b40cf97f8de",
+    "url": "/static/media/bg.9855b418.jpg"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "24c601e721ebd8279d38e2cfa0d01bc6",
+    "url": "/static/media/fontawesome-webfont.24c601e7.svg"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  },
+  {
+    "revision": "23e9fda6d4f40cf7e7b2f843e1f1d325",
+    "url": "/static/media/fontawesome.23e9fda6.ttf"
+  },
+  {
+    "revision": "aa490bfc1d74fee7c14df4c755b86beb",
+    "url": "/static/media/fontawesome.aa490bfc.eot"
+  },
+  {
+    "revision": "bd8f9ffdf97014512f7489c6c5fd176f",
+    "url": "/static/media/fontawesome.bd8f9ffd.svg"
+  },
+  {
+    "revision": "df76bacbecbcf88baa8c6c665c748292",
+    "url": "/static/media/fontawesome.df76bacb.woff"
+  },
+  {
+    "revision": "075e5f7315c616bf2ef5c796e950bb27",
+    "url": "/static/media/icomoon.075e5f73.svg"
+  },
+  {
+    "revision": "3f4414e59b626a73d1b58675d69c3d11",
+    "url": "/static/media/icomoon.3f4414e5.ttf"
+  },
+  {
+    "revision": "5efb47c75c389718d697ff5fa35c343a",
+    "url": "/static/media/icomoon.5efb47c7.woff"
+  },
+  {
+    "revision": "c966e663635ddbe8a470dd24e19a7c39",
+    "url": "/static/media/icomoon.c966e663.eot"
+  },
+  {
+    "revision": "856c1c548743a938a9bcd0105e646e8f",
+    "url": "/static/media/loading.856c1c54.gif"
+  }
+]);
